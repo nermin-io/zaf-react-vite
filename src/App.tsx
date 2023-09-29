fac7a9978c7eaf4fd6e44053338a52f6a2bdb1bd
Nermin <sehicnermin97@gmail.com>
2023-09-29 11:50:11 +1000

    add zaf sdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+import { zafClient } from "./sdk/zafClient.ts";
+
 function App() {
+  useEffect(() => {
+    zafClient.invoke("resize", { height: "400px", width: "100%" });
+  }, []);
   return <div>Hello Zendesk</div>;
 }
 
